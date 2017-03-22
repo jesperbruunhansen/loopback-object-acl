413283b633ab95b4bd72fbc0fd4f3f271186bdff
Jesper Bruun Hansen <jesperbbhansen@gmail.com>
2017-03-22 12:08:17 +0100

    set up tests for group read permissions

--- test/ObjectAcl.e2e.js
+++ test/ObjectAcl.e2e.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const path = require('path');
 const request = require("supertest");
 const Promise = require("bluebird");
-
+const randomstring = require("randomstring");
 const SIMPLE_APP = path.join(__dirname, 'test-server');
 const app = require(path.join(SIMPLE_APP, 'server/server.js'));
 
@@ -57,7 +57,6 @@ describe("Object ACL e2e", () => {
           }
         }),
       ]);
-
     });
 
     it("User 1 has access to Book 1", (done) => {
@@ -101,6 +100,134 @@ describe("Object ACL e2e", () => {
         .expect(200)
         .end(done);
 
+    });
+
+  });
+
+  describe("Group Read permissions", () => {
+
+    let user1token;
+    let user2token;
+
+    let bookId1;
+    let bookId2;
+
+    /**
+     * 1. Create two users
+     *    - First belongs to group: 'aaa'
+     *    - Second belongs to group: 'bbb'
+     *
+     * 2. Log each user in
+     *
+     * 3. Create two Books
+     *    - First has Read ACL to group: 'aaa'
+     *    - Second has Read ACL to group: 'bbb'
+     */
+    before(() => {
+
+      const email1 = randomstring.generate({
+        length: 12,
+        charset: 'alphabetic'
+      });
+      const email2 = randomstring.generate({
+        length: 12,
+        charset: 'alphabetic'
+      });
+
+      return app.models.User.create({
+        email: email1 + "@test.com",
+        password: "1234",
+        acl_groups: ["aaa"]
+      }).then(user1 => {
+
+        return app.models.AccessToken.create({
+          userId: user1.id
+        })
+
+      }).then(accessToken1 => {
+
+        user1token = accessToken1.id;
+
+        return app.models.User.create({
+          email: email2 + "@test.com",
+          password: "1234",
+          acl_groups: ["bbb"]
+        }).then(user2 => {
+
+          return app.models.AccessToken.create({
+            userId: user2.id
+          });
+
+        });
+
+      }).then(accessToken2 => {
+
+        user2token = accessToken2.id;
+
+        //Create books with groups
+        return Promise.all([
+          app.models.Book.create({
+            "name": "name",
+            "isbn": 1231,
+            "$acl": {
+              "r_perm": {
+                "groups": ["aaa"]
+              }
+            }
+          }),
+          app.models.Book.create({
+            "name": "name",
+            "isbn": 1231,
+            "$acl": {
+              "r_perm": {
+                "groups": ["bbb"]
+              }
+            }
+          }),
+        ]).then(books => {
+
+          bookId1 = books[0].id;
+          bookId2 = books[1].id;
+          return Promise.resolve();
+
+        });
+      });
+    });
+
+    it("Returns OK when User 1 accesses Book 1", (done) => {
+      request(app)
+        .get("/api/books/" + bookId1)
+        .set({"authorization": user1token})
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(done);
+    });
+
+    it("Returns OK when User 2 accesses Book 2", (done) => {
+      request(app)
+        .get("/api/books/" + bookId2)
+        .set({"authorization": user2token})
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(done);
+    });
+
+    it("Returns 404 Not Found when User 2 accesses Book 2", done => {
+      request(app)
+        .get("/api/books/" + bookId1)
+        .set({"authorization": user2token})
+        .expect('Content-Type', /json/)
+        .expect(404)
+        .end(done);
+    });
+
+    it("Returns 404 Not Found when User 1 accesses Book 1", done => {
+      request(app)
+        .get("/api/books/" + bookId2)
+        .set({"authorization": user1token})
+        .expect('Content-Type', /json/)
+        .expect(404)
+        .end(done);
     });
 
   });
